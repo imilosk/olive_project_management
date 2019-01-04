2a3f353d366ea6eb2886203a34592c5ac9fb5911
imilosk <milos.kostadinovski97@gmail.com>
2019-01-04 22:08:27 +0200

    #cubqn[IN PROGRESS] Popravljena poimenovanja datotek

--- controllers/api/PSPErrorCategorieController.php
+++ controllers/api/PSPErrorCategorieController.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once __DIR__ . '/../../models/PspErrorCategorie.php';
+require_once __DIR__ . '/../../models/PSPErrorCategories.php';
 
 class PspErrorCategorieController {
 
--- controllers/api/PSPPhaseController.php
+++ controllers/api/PSPPhaseController.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once __DIR__ . '/../../models/PspPhase.php';
+require_once __DIR__ . '/../../models/PSPPhase.php';
 
 class PspPhaseController {
 
